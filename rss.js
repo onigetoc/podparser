1bbebf5c540d05d04c21045b383211985cf6837c
Gino <ginocote@gmail.com>
2017-04-07 11:08:23 -0400

    .content

--- rss.js
+++ rss.js
@@ -106,7 +106,7 @@ module.exports = {
                 obj.thumb_square = 'TEST_IMAGE';
              }
                 //obj.thumb_square = val['media:content'][0]['media:thumbnail'][0].url[0];
-                obj.thumb_square = val.media.thumbnail.url;
+                obj.thumb_square = val.media.content['media:thumbnail'][0].url[0];
             
              if (val['itunes:duration']){
                  obj.duration = val['itunes:duration'][0];
